053a51d89909931422e3b332899d473bbf7c4433
Administrator <gitlab_admin_32c81d@example.com>
2025-05-15 21:43:25 +0000

    Update file ci-build-docker-images.sh

--- ci-build-docker-images.sh
+++ ci-build-docker-images.sh
@@ -9,24 +9,14 @@ log() { echo "$1" >&2; }
 TAG="${TAG:?TAG env variable must be specified}"
 REPO_PREFIX="${REPO_PREFIX:?REPO_PREFIX env variable must be specified}"
 
-while IFS= read -d $'\0' -r dir; do
-    # build image
-    svcname="$(basename "${dir}")"
-    builddir="${dir}"
-    #PR 516 moved cartservice build artifacts one level down to src
-    if [ $svcname == "cartservice" ]
-    then
-        builddir="${dir}/src"
-    fi
-    image="${REPO_PREFIX}/$svcname:$TAG"
-    (
-        cd "${builddir}"
-        log "Building (and pushing) image on GitLab registry: ${image}"
-        buildah images
-        buildah build -t $image
-        buildah images
-        buildah push --tls-verify=false $image
-    )
-done < <(find "${REPO_ROOT}/src" -mindepth 1 -maxdepth 1 -type d -print0)
+# build image
+image="${REPO_PREFIX}/$svcname:$TAG"
+cd "src/frontend"
+
+log "Building (and pushing) image on GitLab registry: ${image}"
+buildah images
+buildah build -t $image
+buildah images
+buildah push --tls-verify=false $image
 
 log "Successfully built and pushed all images."
